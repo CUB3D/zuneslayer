0c260a3147412c7e2f8d4fe31ef2bb3171e97671
CUB3D <callumthom11@gmail.com>
2024-10-02 05:24:17 +0100

    Add zune 30 kernel exploit

---
+++ zuneslayer_kernel/zune30/template/nativeapp/main.cpp
@@ -0,0 +1,33 @@
+﻿/*  _____                         ________   ____    __  __
+ * /\  __`\                      /\_____  \ /\  _`\ /\ \/\ \
+ * \ \ \/\ \  _____     __    ___\/____//'/'\ \ \/\ \ \ \/'/'
+ *  \ \ \ \ \/\ '__`\ /'__`\/' _ `\   //'/'  \ \ \ \ \ \ , <
+ *   \ \ \_\ \ \ \L\ \\  __//\ \/\ \ //'/'___ \ \ \_\ \ \ \\`\
+ *    \ \_____\ \ ,__/ \____\ \_\ \_\/\_______\\ \____/\ \_\ \_\
+ *     \/_____/\ \ \/ \/____/\/_/\/_/\/_______/ \/___/  \/_/\/_/
+ *              \ \_\
+ *               \/_/ OpenZDK Release 1 | 2010-04-14
+ *
+ * main.cpp
+ * Copyright (c) 2010 itsnotabigtruck.
+ * No rights reserved.
+ * 
+ * All rights are waived to the maximum extent possible; see
+ * http://creativecommons.org/publicdomain/zero/1.0/ for more information
+ *
+ * Replace this banner when writing your own applications
+ */
+
+#include <windows.h>
+#include <zdk.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
+{
+	int a = 0;
+	while(1) {
+a++;
+	}
+	int i = GENERIC_READ;
+   // MessageBox(NULL, L"HACK THE PLANET!", L"Hackage has occurred", MB_OK);
+    return 0x69;
+}
